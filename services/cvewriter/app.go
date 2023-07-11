c9ac04c5f7f45e2fb7ae2c873a47e88bc155f1f7
GoodmanDev <david.g.goodman@bath.edu>
2023-07-11 17:51:42 +0100

    Add mongodb connection & writing

--- services/cvewriter/app.go
+++ services/cvewriter/app.go
@@ -7,11 +7,21 @@ import (
 	"os"
 
 	"github.com/segmentio/kafka-go"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var kafkaReader *kafka.Reader
+var dbClient *mongo.Client
+var dbCollection *mongo.Collection
 
 func init() {
+
+	// connect to Kafka broker
+
 	kafkaServer := readFromENV("KAFKA_BROKER", "localhost:9092")
 	kafkaTopic := readFromENV("KAFKA_TOPIC", "nvd-cves")
 
@@ -20,20 +30,72 @@ func init() {
 	fmt.Println("Kafka Broker - ", kafkaServer)
 	fmt.Println("Kafka Topic - ", kafkaTopic)
 
+	// connect to mongodb
+
+	mongoServer := readFromENV("MONGO_URL", "mongodb://localhost:27017")
+	mongoDatabaseName := readFromENV("MONGO_DB", "melaka")
+	mongoCollectionName := readFromENV("MONGO_COLLECTION", "cves")
+
+	credentials := options.Credential{
+		Username: readFromENV("MONGO_ROOT_USERNAME", "dev"),
+		Password: readFromENV("MONGO_ROOT_PASSWORD", "dev"),
+	}
+
+	var connErr error
+
+	dbClient, connErr = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoServer).SetAuth(credentials))
+	if connErr != nil {
+		log.Fatalf("Instantiation of db connection failed, %s", connErr)
+	}
+
+	// ping db to test the connection
+	if pingErr := dbClient.Ping(context.TODO(), readpref.Primary()); pingErr != nil {
+		log.Fatalf("Pinging db failed, %s", pingErr)
+	}
+
+	// set up our collection
+	dbCollection = dbClient.Database(mongoDatabaseName).Collection(mongoCollectionName)
+
 }
 
 func main() {
+
+	defer kafkaReader.Close()
+
 	for {
-		m, err := kafkaReader.ReadMessage(context.Background())
-		if err != nil {
-			break
+		m, readErr := kafkaReader.ReadMessage(context.Background())
+		if readErr != nil {
+			continue
 		}
-		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+		//fmt.Printf("Message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+
+		e := handleMsg(m)
+		if e != nil {
+			fmt.Printf("Failed to handle message, %s", e)
+		}
 	}
 
-	if err := kafkaReader.Close(); err != nil {
-		log.Fatal("failed to close reader:", err)
+}
+
+func handleMsg(msg kafka.Message) error {
+
+	fmt.Printf("Read message from broker, key %s, value %s", string(msg.Key), string(msg.Value))
+
+	var bdoc interface{}
+	unmarshalErr := bson.UnmarshalExtJSON(msg.Value, false, &bdoc)
+
+	if unmarshalErr != nil {
+		return unmarshalErr
 	}
+	result, insertErr := dbCollection.InsertOne(context.TODO(), &bdoc)
+
+	if insertErr != nil {
+		return insertErr
+	}
+
+	fmt.Printf("%s", result.InsertedID)
+
+	return nil
 }
 
 func newKafkaReader(kafkaURL, topic string) *kafka.Reader {
