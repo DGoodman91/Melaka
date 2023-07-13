2c02c3cd43f99e24b5ef338874d6fcbf12d4c55f
GoodmanDev <david.g.goodman@bath.edu>
2023-07-14 00:55:50 +0100

    Refactoring of some Go code to tidy things up

--- services/cvewriter/app.go
+++ services/cvewriter/app.go
@@ -7,31 +7,26 @@ import (
 	"os"
 
 	"github.com/segmentio/kafka-go"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var kafkaReader *kafka.Reader
-var dbClient *mongo.Client
-var dbCollection *mongo.Collection
+var (
+	kafkaReader  *kafka.Reader
+	dbCollection *mongo.Collection
+)
 
 func init() {
-
-	// connect to Kafka broker
-
+	// Connect to Kafka broker
 	kafkaServer := readFromENV("KAFKA_BROKER", "localhost:9092")
 	kafkaTopic := readFromENV("KAFKA_TOPIC", "nvd-cves")
-
 	kafkaReader = newKafkaReader(kafkaServer, kafkaTopic)
-
 	fmt.Println("Kafka Broker - ", kafkaServer)
 	fmt.Println("Kafka Topic - ", kafkaTopic)
 
-	// connect to mongodb
-
+	// Connect to MongoDB
 	mongoServer := readFromENV("MONGO_URL", "mongodb://localhost:27017")
 	mongoDatabaseName := readFromENV("MONGO_DB", "melaka")
 	mongoCollectionName := readFromENV("MONGO_COLLECTION", "cves")
@@ -41,60 +36,49 @@ func init() {
 		Password: readFromENV("MONGO_ROOT_PASSWORD", "dev"),
 	}
 
-	var connErr error
-
-	dbClient, connErr = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoServer).SetAuth(credentials))
-	if connErr != nil {
-		log.Fatalf("Instantiation of db connection failed, %s", connErr)
+	dbClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoServer).SetAuth(credentials))
+	if err != nil {
+		log.Fatalf("Instantiation of db connection failed: %s", err)
 	}
 
-	// ping db to test the connection
-	if pingErr := dbClient.Ping(context.TODO(), readpref.Primary()); pingErr != nil {
-		log.Fatalf("Pinging db failed, %s", pingErr)
+	// Ping db to test the connection
+	if err := dbClient.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatalf("Pinging db failed: %s", err)
 	}
 
-	// set up our collection
 	dbCollection = dbClient.Database(mongoDatabaseName).Collection(mongoCollectionName)
-
 }
 
 func main() {
-
 	defer kafkaReader.Close()
 
 	for {
-		m, readErr := kafkaReader.ReadMessage(context.Background())
-		if readErr != nil {
+		m, err := kafkaReader.ReadMessage(context.Background())
+		if err != nil {
 			continue
 		}
 		//fmt.Printf("Message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
 
-		e := handleMsg(m)
-		if e != nil {
-			fmt.Printf("Failed to handle message, %s", e)
+		if err := handleMsg(m); err != nil {
+			fmt.Printf("Failed to handle message: %s", err)
 		}
 	}
-
 }
 
 func handleMsg(msg kafka.Message) error {
-
 	fmt.Printf("Read message from broker, key %s, value %s", string(msg.Key), string(msg.Value))
 
 	var bdoc interface{}
-	unmarshalErr := bson.UnmarshalExtJSON(msg.Value, false, &bdoc)
-
-	if unmarshalErr != nil {
-		return unmarshalErr
+	if err := bson.UnmarshalExtJSON(msg.Value, false, &bdoc); err != nil {
+		return err
 	}
-	result, insertErr := dbCollection.InsertOne(context.TODO(), &bdoc)
 
-	if insertErr != nil {
-		return insertErr
+	result, err := dbCollection.InsertOne(context.TODO(), bdoc)
+	if err != nil {
+		return err
 	}
 
 	fmt.Printf("%s", result.InsertedID)
-
 	return nil
 }
 
@@ -109,9 +93,8 @@ func newKafkaReader(kafkaURL, topic string) *kafka.Reader {
 }
 
 func readFromENV(key, defaultVal string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultVal
+	if value := os.Getenv(key); value != "" {
+		return value
 	}
-	return value
+	return defaultVal
 }
--- services/nvdscraper/app.go
+++ services/nvdscraper/app.go
@@ -14,16 +14,17 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-var kafkaWriter *kafka.Writer
-var httpClient *http.Client
-var nvdAPIKey string
+var (
+	kafkaWriter *kafka.Writer
+	httpClient  *http.Client
+	nvdAPIKey   string
+)
 
 func init() {
 	kafkaServer := readFromENV("KAFKA_BROKER", "localhost:9092")
 	kafkaTopic := readFromENV("KAFKA_TOPIC", "nvd-cves")
 
 	kafkaWriter = newKafkaWriter(kafkaServer, kafkaTopic)
-
 	fmt.Println("Kafka Broker - ", kafkaServer)
 	fmt.Println("Kafka Topic - ", kafkaTopic)
 
@@ -34,113 +35,99 @@ func init() {
 
 func newKafkaWriter(kafkaURL, topic string) *kafka.Writer {
 	return &kafka.Writer{
-		Addr:     kafka.TCP(kafkaURL),
+		Addr:     kafka.TCP(kafkaURL), // TODO handling for multiple brokers
 		Topic:    topic,
 		Balancer: &kafka.LeastBytes{},
 	}
 }
 
 func main() {
-
 	defer kafkaWriter.Close()
-
 	time.Sleep(10 * time.Second) // TODO this is a temp hack to wait for kafka to start accepting before we have proper connection/retry handling
-
-	// initialize out data set
-	// TODO do we actually need to, or has it already been done?
 
 	startIndex := 0
 	totalResults := 0
 
-	for done := false; !done; done = (startIndex > totalResults) {
+	// TODO how do we persist the fact that initialization is complete?
+	// TODO once initialization is complete, need to start periodically checking for updates using the lastModifiedDate parameter
+	// NVD docs suggest doing this no more than once every two hours - could be better served by a serverless function that runs sporadically instead of a long-running one
 
-		// TODO retry handling
-		req, reqErr := http.NewRequest("GET", "https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex="+strconv.Itoa(startIndex), nil)
-		if reqErr != nil {
-			log.Fatalf("HTTP request creation failed: %s", reqErr) // Fatalln prints then exits
-		}
-
-		if nvdAPIKey != "" {
-			req.Header.Add("apiKey", nvdAPIKey)
-		}
-
-		resp, getErr := httpClient.Do(req)
-		if getErr != nil {
-			log.Fatalf("HTTP request failed: %s", getErr) // TODO need retries, not built into golang's http.client though https://medium.com/@nitishkr88/http-retries-in-go-e622e51d249f
+	for startIndex <= totalResults {
+		resp, err := sendHTTPRequest("GET", "https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex="+strconv.Itoa(startIndex), nil)
+		if err != nil {
+			log.Fatalf("HTTP request failed: %s", err)
 		}
 		defer resp.Body.Close()
 
-		if resp.StatusCode != 200 {
-			respMsg := ""
-			val, ok := resp.Header["Message"]
-			if ok && len(val) > 0 {
-				respMsg = resp.Header["Message"][0]
-			}
-			log.Fatalf("Unexpected response from NVD, response code %s, message %s", strconv.Itoa(resp.StatusCode), respMsg)
+		if resp.StatusCode != http.StatusOK {
+			respMsg := resp.Header.Get("Message")
+			log.Fatalf("Unexpected response from NVD, response code %d, message: %s", resp.StatusCode, respMsg)
 		}
 
-		// pull the response body out and deserialize it into a Response obj
-		body, ioReadErr := io.ReadAll(resp.Body)
-		if ioReadErr != nil {
-			log.Fatalf("Failed to read body from HTTP response: %s", ioReadErr)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Failed to read body from HTTP response: %s", err)
 		}
 
+		// Pull the response body out and deserialize it into a Response obj
 		var result Response
-		if unmarshalErr := json.Unmarshal(body, &result); unmarshalErr != nil {
-			log.Fatalf("Failed to parse response body into JSON: %s", unmarshalErr)
+		if err := json.Unmarshal(body, &result); err != nil {
+			log.Fatalf("Failed to parse response body into JSON: %s", err)
 		}
 
-		// send each of the CVE data elements to kafka
-
-		for i := 0; i < len(result.Vulnerabilities); i++ {
-
-			cve := result.Vulnerabilities[i].Cve
-
-			cveMsg, cveMsgErr := NewCveMsg(cve, result.Timestamp)
-			if cveMsgErr != nil {
-				log.Printf("Error generating CveMsg instance for data %s, %s", cve.ID, cveMsgErr)
+		// Send each of the CVE data elements to kafka
+		for _, vulnerability := range result.Vulnerabilities {
+			cve := vulnerability.Cve
+			cveMsg, err := NewCveMsg(cve, result.Timestamp)
+			if err != nil {
+				log.Printf("Error generating CveMsg instance for data %s: %s", cve.ID, err)
 				continue
 			}
 
-			value, marshalErr := json.Marshal(cveMsg)
-			if marshalErr != nil {
-				log.Printf("Failed to serialize CVE data into JSON: %s", marshalErr)
+			value, err := json.Marshal(cveMsg)
+			if err != nil {
+				log.Printf("Failed to serialize CVE data into JSON: %s", err)
 				continue
 			}
 
-			key := result.Vulnerabilities[i].Cve.ID
+			key := cve.ID
 			msg := kafka.Message{
 				Key:   []byte(key),
 				Value: []byte(value),
 			}
-			writerError := kafkaWriter.WriteMessages(context.Background(), msg)
-			if writerError != nil {
-				fmt.Printf("Failed to enqueue message %s: %s\n", value, writerError)
+
+			if err := kafkaWriter.WriteMessages(context.Background(), msg); err != nil {
+				log.Printf("Failed to enqueue message %s: %s\n", value, err)
 				continue
 			} else {
 				fmt.Printf("Enqueued data for CVE %s\n", key)
 			}
-
 		}
 
 		totalResults = result.TotalResults
-		startIndex = startIndex + result.ResultsPerPage
+		startIndex += result.ResultsPerPage
 
 		time.Sleep(10 * time.Second)
+	}
+}
 
+// TODO retry handling
+func sendHTTPRequest(method, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
 	}
 
-	// TODO how do we persist the fact that initialization is complete?
+	if nvdAPIKey != "" {
+		req.Header.Add("apiKey", nvdAPIKey)
+	}
 
-	// TODO once initialization is complete, need to start periodically checking for updates using the lastModifiedDate parameter
-	// NVD docs suggest doing this no more than once every two hours - could be better served by a serverless function that runs sporadically instead of a long-running one
-
+	return httpClient.Do(req)
 }
 
 func readFromENV(key, defaultVal string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultVal
+	if value := os.Getenv(key); value != "" {
+		return value
 	}
-	return value
+	return defaultVal
 }
